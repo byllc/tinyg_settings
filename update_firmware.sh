6113a8e0881abacf6256d1b32c492a9a0b46c214
Bill Chapman <wwchapman@gmail.com>
2015-01-07 13:39:17 -0500

    added latest stable firmware

--- update_firmware.sh
+++ update_firmware.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#http://synthetos.github.io/
 LATEST_FIRMWARE=tinyg-edge-438.02.hex
+
 #avrdude installed with homebrew
 avrdude -p x192a3 -c avr109 -b 115200 -P /dev/tty.usbserial-DA00G610 -e -U flash:w:${LATEST_FIRMWARE}
